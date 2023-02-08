9b2a38d7d7ff7ddd2a0304ecf2e5dd32057e481a
rubyazz <ersultan.abduvalov@gmail.com>
2023-02-08 22:14:42 +0600

    field model

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('', views.home, name=home)
+    path('', views.home, name='home'),
 ]
